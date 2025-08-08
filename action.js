0168f4fedb25f35003f6d0b286733ef8725da99c
Luis Guilherme <guilherme32698@gmail.com>
2025-08-08 16:50:32 -0300

    funcionalidade principal funcionando / trocando imagem de fundo

--- action.js
+++ action.js
@@ -1,14 +1,15 @@
-const InputValue = document.getElementById("valor");
-const InputDate = document.getElementById("time");
-const InputBottom = document.getElementById("bottom");
+document.getElementById("Form").addEventListener("submit", function (event) {
+event.preventDefault();
+  const InputValue = document.getElementById("valor");
+  const InputDate = document.getElementById("time");
+  const InputBottom = document.getElementById("bottom");
 
-const Valor = InputValue.value;
-const Botao = InputBottom.value;
-    
-    if(Botao == true){
-        console.log(Valor);
-    }
+  const Valor = InputValue.value;
+  const Tempo = InputDate.value;
+  const Botao = InputBottom.value;
 
-    
-    
-    
+  var Resultado = Valor * Tempo;
+  var resto = 
+  document.getElementById("resultado").textContent = "A quantia que você juntará será de: R$" + Resultado;
+
+});
